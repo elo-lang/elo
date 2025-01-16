6800727ccb415d7ab174387590ee8acd23721020
marc-dantas <marciodantasms@hotmail.com>
2025-01-16 15:44:41 -0300

    Make content Chars<'_> a Peekable iterable in InputFile

--- components/oxido-ir/src/inputfile.rs
+++ components/oxido-ir/src/inputfile.rs
@@ -1,8 +1,9 @@
 use crate::word::Word;
 use std::str::Chars;
+use std::iter::Peekable;
 
 #[derive(Debug, Clone)]
 pub struct InputFile<'a> {
     pub filename: Word,
-    pub content: Chars<'a>,
+    pub content: Peekable<Chars<'a>>,
 }
