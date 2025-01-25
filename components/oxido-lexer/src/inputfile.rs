45d6dcd9537a2cb251638cbf2377110c77820d61
marc-dantas <marciodantasms@hotmail.com>
2025-01-25 17:00:38 -0300

    added constructor for InputFile

--- components/oxido-lexer/src/inputfile.rs
+++ components/oxido-lexer/src/inputfile.rs
@@ -6,3 +6,12 @@ pub struct InputFile<'a> {
     pub filename: &'static str,
     pub content: Peekable<Chars<'a>>,
 }
+
+impl<'a> InputFile<'a> {
+    pub fn new(filename: &'static str, content: Chars<'a>) -> InputFile<'a> {
+        InputFile {
+            filename,
+            content: content.peekable(),
+        }
+    }
+}
--- components/oxido-lexer/src/main.rs
+++ components/oxido-lexer/src/main.rs
@@ -6,10 +6,10 @@ fn main() {
     // Tive que remover os testes, perdão igor :(
     // Não sabia como rodar os testes de um projeto de dentro de outro
     let source_text = "let x = 1..5"; // Apparently it works
-    let lx = Lexer::new(InputFile {
-        filename: "main.rs",
-        content: source_text.chars().peekable(),
-    });
+    let lx = Lexer::new(InputFile::new(
+        "main.rs",
+        source_text.chars(),
+    ));
 
     let tokens: Vec<Token> = lx.collect();
     println!("{:?}", tokens);
