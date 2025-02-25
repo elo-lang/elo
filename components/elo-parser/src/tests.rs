cc29387c2395d0e6a810260292e7146a4077e965
marc-dantas <marciodantasms@hotmail.com>
2025-02-25 18:19:34 -0300

    test_file test case

--- components/elo-parser/src/tests.rs
+++ components/elo-parser/src/tests.rs
@@ -3,6 +3,24 @@ use elo_lexer::inputfile::InputFile;
 use elo_lexer::lexer::Lexer;
 
 #[test]
+fn test_file() {
+    use std::fs::read_to_string;
+    let filename = "test.elo";
+    let source_text = &read_to_string(filename).unwrap();
+    let lx = Lexer::new(InputFile::new("test.rs", source_text));
+
+    let mut parser = Parser::new(lx);
+    match parser.parse() {
+        Ok(ast) => {
+            println!("{:#?}", ast);
+        }
+        Err(e) => {
+            println!("{e:?}");
+        }
+    }
+}
+
+#[test]
 fn test_binop() {
     let source_text = "(a + b * c) / 2";
     let lx = Lexer::new(InputFile::new("test.rs", source_text));
