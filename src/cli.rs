22b0fab481d1c07e44c772f57cc3aeaafc14d768
marc-dantas <marciodantasms@hotmail.com>
2025-06-25 20:11:46 -0300

    proper command line argument parsing and basic functioning

---
+++ src/cli.rs
@@ -0,0 +1,221 @@
+
+pub fn fatal(program: &str, msg: &str) {
+    eprintln!("{program}: fatal: {msg}");
+}
+
+pub fn warning(program: &str, msg: &str) {
+    eprintln!("{program}: warning: {msg}");
+}
+
+pub fn information(program: &str, msg: &str) {
+    eprintln!("{program}: info: {msg}");
+}
+
+pub fn usage(program: &str, command: Option<&Command>) {
+    if let Some(cmd) = command {
+        match cmd {
+            Command::Run { .. } => eprintln!("usage: {program} run <input>"),
+            Command::Build { .. } => eprintln!("usage: {program} build <input> [-o <output>] [-O0|-O1|-O2|-O3]"),
+            Command::Help { .. } => eprintln!("usage: {program} help [<command>]"),
+        }
+    } else {
+        eprintln!("usage: {program} <command> ...");
+    }
+}
+
+pub fn error(program: &str, msg: &str) -> ! {
+    eprintln!("{program}: error: {msg}");
+    std::process::exit(1);
+}
+
+pub fn help(program: &str, command: Option<&Command>) {
+    usage(program, command);
+    match command {
+        Some(Command::Help { .. }) | None => {
+            eprintln!("commands:");
+            eprintln!("    run        Run with the given input file");
+            eprintln!("    build      Build from given source code");
+            eprintln!("    help       Show help message for a specific command or general help");
+        }
+        Some(Command::Run { .. }) => {
+            eprintln!("\nBuild and automatically run the given input file\n");
+            eprintln!("positional arguments:");
+            eprintln!("    <input>          Source code input file to run");
+        }
+        Some(Command::Build { .. }) => {
+            eprintln!("\nBuild the given source code input file\n");
+            eprintln!("positional arguments:");
+            eprintln!("    <input>          Source code input file to build");
+            eprintln!("flags:");
+            eprintln!("    -o <output>      Specify output file");
+            eprintln!("    -O0              Set optimization level to 0");
+            eprintln!("    -O1              Set optimization level to 1 (default)");
+            eprintln!("    -O2              Set optimization level to 2");
+            eprintln!("    -O3              Set optimization level to 3");
+        }
+    }
+}
+
+pub enum O {
+    None,       // -O0
+    Normal,     // -O1
+    Medium,     // -O2
+    Aggressive, // -O3
+}
+
+pub enum Command {
+    Build {
+        input: String,
+        output: Option<String>,
+        optimization: O,
+    },
+    Run {
+        input: String,
+    },
+    Help {
+        command: Option<String>,
+    },
+}
+
+impl Command {
+    pub fn from_str(command: &str) -> Option<Self> {
+        match command {
+            "build" => Some(Command::Build {
+                input: String::new(),
+                output: None,
+                optimization: O::Normal,
+            }),
+            "run" => Some(Command::Run {
+                input: String::new(),
+            }),
+            "help" => Some(Command::Help { command: None }),
+            _ => None,
+        }
+    }
+}
+
+fn parse_run(program: &str, args: &[String]) -> Result<Command, ()> {
+    if args.len() < 2 {
+        return Err(());
+    }
+
+    let input = None;
+
+    for arg in args.iter().skip(1) {
+        match arg.as_str() {
+            _ if input.is_none() => {
+                return Ok(Command::Run {
+                    input: arg.to_string(),
+                });
+            }
+            _ if input.is_some() => {
+                usage(program, Command::from_str("run").as_ref());
+                fatal(program, &format!("unexpected positional argument `{arg}`"));
+                return Err(());
+            }
+            _ => {
+                usage(program, Command::from_str("run").as_ref());
+                fatal(program, "expected positional argument: <input>");
+                return Err(());
+            }
+        }
+    }
+    
+    if input.is_none() {
+        usage(program, Command::from_str("build").as_ref());
+        fatal(program, "expected positional argument: <input>");
+        return Err(());
+    }
+    Ok(Command::Run {
+        input: input.unwrap(),
+    })
+}
+
+fn parse_build(program: &str, args: &[String]) -> Result<Command, ()> {
+    if args.len() < 2 {
+        return Err(());
+    }
+
+    let mut input = None;
+    let mut output = None;
+    let mut optimization = O::Normal;
+
+    for arg in args.iter().skip(2) {
+        match arg.as_str() {
+            "-O0" => optimization = O::None,
+            "-O1" => optimization = O::Normal,
+            "-O2" => optimization = O::Medium,
+            "-O3" => optimization = O::Aggressive,
+            _ if arg.starts_with("-o") => {
+                let rest= arg[2..].to_string();
+                if (rest.is_empty()) {
+                    // get the next argument instead
+                    if let Some(next_arg) = args.iter().skip_while(|x| x != &arg).nth(1) {
+                        output = Some(next_arg.to_string());
+                    } else {
+                        usage(program, Command::from_str("build").as_ref());
+                        fatal(program, "expected output file after `-o` flag");
+                        return Err(());
+                    }
+                } else {
+                    output = Some(rest);
+                }
+            }
+            _ if input.is_none() => {
+                input = Some(arg.to_string());
+            }
+            x if input.is_some() => {
+                usage(program, Command::from_str("build").as_ref());
+                fatal(program, &format!("unexpected positional argument `{x}`"));
+                return Err(());
+            }
+            _ => {}
+        }
+    }
+
+    if input.is_none() {
+        usage(program, Command::from_str("build").as_ref());
+        fatal(program, "expected positional argument: <input>");
+        return Err(());
+    }
+    Ok(Command::Build {
+        input: input.unwrap(),
+        output,
+        optimization,
+    })
+} 
+
+fn parse_help(args: &[String]) -> Result<Command, ()> {
+    Ok(Command::Help {
+        command: args.iter().skip(2).next().map(|s| s.to_string()),
+    })
+}
+
+fn parse_command(program: &str, args: &[String]) -> Result<Command, ()> {
+    let command = &args[1];
+    match command.as_str() {
+        "r" | "run" => parse_run(program, args),
+        "b" | "build" => parse_build(program, args),
+        "h" | "help" => parse_help(args),
+        _ => {
+            usage(program, None);
+            fatal(program, &format!("unknown command `{command}`"));
+            Err(())
+        }
+    }
+}
+
+pub fn parse_args(args: &[String]) -> Result<Command, ()> {
+    if args[1..].is_empty() {
+        usage(&args[0], None);
+        fatal(&args[0], "expected command");
+        information(&args[0], "run with `help` command to see available commands");
+        return Err(());
+    }
+
+    let program = &args[0];
+    match parse_command(program, args) {
+        Ok(action) => Ok(action),
+        Err(..) => { Err(()) }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ use elo_ast::ast;
 use elo_ir::ir;
 use elo_error::{parseerror, typeerror};
 
-use inkwell::module::Module;
+mod cli;
+use crate::cli::*;
+
 use inkwell::targets::{InitializationConfig, Target, TargetMachine, RelocMode, CodeModel, FileType};
 use inkwell::OptimizationLevel;
 
@@ -27,68 +29,82 @@ fn validate_program(prog: ast::Program) -> Result<ir::ValidatedProgram, typeerro
 }
 
 fn main() {
-    let args = args().collect::<Vec<String>>();
-    if args.len() < 2 {
-        println!("usage: {} FILENAME", args[0]);
-        std::process::exit(1);
-    }
+    let args: Vec<String> = args().collect();
+    let comm = parse_args(&args).unwrap_or_else(|_| {
+        cli::error(&args[0], "could not parse command line arguments");
+    });
 
-    let filename = &args[1];
-    if let Some(content) = std::fs::read_to_string(filename).ok() {
-        let input_file = InputFile {
-            filename: filename.as_str(),
-            content: content.as_str(),
-        };
-        match parse_program(input_file) {
-            Ok(program) => {
-                match validate_program(program) {
-                    Ok(validated_program) => {
-                        let context = inkwell::context::Context::create();
-                        let module = context.create_module(filename);
-                        let mut r#gen = Generator {
-                            input: validated_program,
-                            context: &context,
-                            module: module,
-                            builder: context.create_builder(),
-                            namespace: Namespace {
-                                locals: HashMap::new(),
-                                constants: HashMap::new(),
-                            },
-                        };
-                        r#gen.generate();
-                        Target::initialize_native(&InitializationConfig::default())
-                            .expect("Failed to initialize native target");
+    match comm {
+        Command::Build { input, output, optimization } => {
+            if let Some(content) = std::fs::read_to_string(&input).ok() {
+                let input_file = InputFile {
+                    filename: input.as_str(),
+                    content: content.as_str(),
+                };
+                match parse_program(input_file) {
+                    Ok(program) => {
+                        match validate_program(program) {
+                            Ok(validated_program) => {
+                                let context = inkwell::context::Context::create();
+                                let module = context.create_module(&input);
+                                let mut r#gen = Generator {
+                                    input: validated_program,
+                                    context: &context,
+                                    module: module,
+                                    builder: context.create_builder(),
+                                    namespace: Namespace {
+                                        locals: HashMap::new(),
+                                        constants: HashMap::new(),
+                                    },
+                                };
+                                r#gen.generate();
+                                Target::initialize_native(&InitializationConfig::default())
+                                    .expect("Failed to initialize native target");
 
-                        let triple = TargetMachine::get_default_triple();
-                        let target = Target::from_triple(&triple).unwrap();
-                        let cpu = "generic";
-                        let features = "";
-                        let opt_level = OptimizationLevel::Aggressive;
-                        let reloc = RelocMode::PIC;
-                        let code_model = CodeModel::Default;
-                        let target_file_type = FileType::Object;
+                                let triple = TargetMachine::get_default_triple();
+                                let target = Target::from_triple(&triple).unwrap();
+                                let cpu = "generic";
+                                let features = "";
+                                let opt_level = match optimization {
+                                    cli::O::None => OptimizationLevel::None,
+                                    cli::O::Normal => OptimizationLevel::Less,
+                                    cli::O::Medium => OptimizationLevel::Default,
+                                    cli::O::Aggressive => OptimizationLevel::Aggressive,
+                                };
+                                let reloc = RelocMode::PIC;
+                                let code_model = CodeModel::Default;
+                                let target_file_type = FileType::Object;
 
-                        let target_machine = target
-                            .create_target_machine(&triple, cpu, features, opt_level, reloc, code_model)
-                            .expect("Failed to create target machine");
+                                let target_machine = target
+                                    .create_target_machine(&triple, cpu, features, opt_level, reloc, code_model)
+                                    .expect("Failed to create target machine");
 
-                        let path = format!("{}.o", filename);
-                        let path = Path::new(&path);
-                        target_machine
-                            .write_to_file(&r#gen.module, target_file_type, &path)
-                            .expect("Failed to write object file");
+                                let path = format!("{}.o", output.unwrap_or_else(|| input.clone()));
+                                let path = Path::new(&path);
+                                target_machine
+                                    .write_to_file(&r#gen.module, target_file_type, &path)
+                                    .expect("Failed to write object file");
+                            }
+                            Err(e) => {
+                                typeerror::type_error(e.case, &e.span.unwrap().into_filespan(input_file));
+                            }
+                        }
                     }
                     Err(e) => {
-                        typeerror::type_error(e.case, &e.span.unwrap().into_filespan(input_file));
+                        parseerror::parse_error(e.case, &e.span.unwrap().into_filespan(input_file));
                     }
                 }
-            }
-            Err(e) => {
-                parseerror::parse_error(e.case, &e.span.unwrap().into_filespan(input_file));
+            } else {
+                cli::error(&args[0], &format!("could not read input file {}", input));
             }
         }
-    } else {
-        eprintln!("error: could not read file {}", filename);
-        std::process::exit(1);
+        Command::Help { command } => {
+            if let Some(command) = &command {
+                help(&args[0], Command::from_str(command).as_ref());
+            } else {
+                help(&args[0], None);
+            }
+        }
+        _ => {}
     }
 }
