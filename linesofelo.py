65457db616fecc953c2b85b0d4005d7e5c8b202f
marc-dantas <marciodantasms@hotmail.com>
2026-01-27 13:52:55 -0300

    improved linesofelo.py script just for fun

--- linesofelo.py
+++ linesofelo.py
@@ -5,28 +5,43 @@ from os import listdir
 SOURCE_PATH = "compiler/"
 
 
-def read_source_code_lines(dirpath: str) -> int:
-    lines = 0
+def read_source_code_info(dirpath: str) -> dict[str, int] | None:
+    info = {
+        "lines": 0,
+        "semis": 0,
+        "curly": 0,
+    }
     if not isdir(dirpath):
-        return -1
+        return None
 
     for p in listdir(dirpath):
         if isdir(new_dir := join(dirpath, p)):
-            lines += read_source_code_lines(new_dir)
+            new_info = read_source_code_info(new_dir)
+            if new_info is None:
+                return None
+            info = {
+                "lines": info["lines"] + new_info["lines"],
+                "semis": info["semis"] + new_info["semis"],
+                "curly": info["curly"] + new_info["curly"],
+            }
         if p.endswith(".rs"):
             print(p)
             with open(join(dirpath, p), "r") as f:
-                lines += len(f.readlines())
-
-    return lines
+                for char in f.read():
+                    info["curly"] += int(char in ('{', '}'))
+                    info["semis"] += int(char == ';')
+                    info["lines"] += int(char == '\n')
+    return info
 
 
 def main():
-    x = read_source_code_lines(SOURCE_PATH)
-    if x == -1:
+    info = read_source_code_info(SOURCE_PATH)
+    if info is None:
         print("source path is not a directory")
         return
-    print(f"lines of elo: {x}")
+    print(f"lines of elo: {info['lines']}")
+    print(f"curly braces of elo: {info['curly']}")
+    print(f"semicolons of elo: {info['semis']}")
 
 
 if __name__ == "__main__":
