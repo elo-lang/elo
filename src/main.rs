a8f3ac97ec018a33ec1766d98cef577f2b04381e
marc-dantas <marciodantasms@hotmail.com>
2026-02-07 18:02:49 -0300

    main: got rid of callback function in parse_and_validate helper function

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ fn strip_extension(path: &str) -> String {
     }
 }
 
-fn parse_and_validate(filename: &str, source: &str, mut callback: impl FnMut(cir::Program)) {
+fn parse_and_validate(filename: &str, source: &str) -> cir::Program {
     let input_file = InputFile {
         filename,
         content: source,
@@ -47,7 +47,7 @@ fn parse_and_validate(filename: &str, source: &str, mut callback: impl FnMut(cir
     match parse_program(input_file) {
         Ok(program) => match validate_program(program) {
             Ok(validated_program) => {
-                callback(validated_program);
+                return validated_program;
             }
             Err(es) => {
                 let error_amount = es.len() as i32;
@@ -85,30 +85,29 @@ fn main() {
                 let output_exe = output.unwrap_or(format!("{}.out", input_name));
                 let output_c = format!("{}.c", input_name);
                 let output_h = format!("{}.h", input_name);
-                parse_and_validate(input.as_str(), content.as_str(), |validated_program| {
-                    tcc.set_output_type(tcc::OutputType::Executable);
-                    let mut r#gen = Generator::new(validated_program);
-                    r#gen.go();
-                    let generated_output = &format!("{}{}", r#gen.head, r#gen.body);
-                    if c {
-                        if h {
-                            std::fs::write(&output_c, r#gen.body).unwrap();
-                        } else {
-                            std::fs::write(&output_c, generated_output).unwrap();
-                        }
-                    }
+                let validated_program = parse_and_validate(input.as_str(), content.as_str());
+                tcc.set_output_type(tcc::OutputType::Executable);
+                let mut r#gen = Generator::new(validated_program);
+                r#gen.go();
+                let generated_output = &format!("{}{}", r#gen.head, r#gen.body);
+                if c {
                     if h {
-                        std::fs::write(&output_h, r#gen.head).unwrap();
+                        std::fs::write(&output_c, r#gen.body).unwrap();
+                    } else {
+                        std::fs::write(&output_c, generated_output).unwrap();
                     }
-                    if !c && !h {
-                        if tcc.compile_string(generated_output).is_err() {
-                            cli::critical("could not compile C backend source-code. This is likely a bug");
-                            cli::information("if so, please report the bug at https://github.com/elo-lang/elo/issues");
-                            std::process::exit(-1);
-                        }
-                        tcc.output_file(&output_exe);
+                }
+                if h {
+                    std::fs::write(&output_h, r#gen.head).unwrap();
+                }
+                if !c && !h {
+                    if tcc.compile_string(generated_output).is_err() {
+                        cli::critical("could not compile C backend source-code. This is likely a bug");
+                        cli::information("if so, please report the bug at https://github.com/elo-lang/elo/issues");
+                        std::process::exit(-1);
                     }
-                });
+                    tcc.output_file(&output_exe);
+                }
             } else {
                 cli::fatal(&format!("could not read input file {}", input));
                 std::process::exit(-1);
@@ -126,18 +125,17 @@ fn main() {
             args: arguments,
         } => {
             if let Some(content) = std::fs::read_to_string(&input).ok() {
-                parse_and_validate(input.as_str(), content.as_str(), |validated_program| {
-                    tcc.set_output_type(tcc::OutputType::Memory);
-                    let g = generate_program(validated_program);
-                    if tcc.compile_string(&g).is_err() {
-                        cli::critical("could not compile C backend source-code. This is likely a bug");
-                        cli::information("if so, please report the bug at https://github.com/elo-lang/elo/issues");
-                        std::process::exit(-1);
-                    }
-                    let arguments =
-                        arguments.iter().map(|x| x.as_str()).collect::<Vec<&str>>();
-                    std::process::exit(tcc.run(&arguments));
-                });
+                let validated_program = parse_and_validate(input.as_str(), content.as_str());
+                tcc.set_output_type(tcc::OutputType::Memory);
+                let g = generate_program(validated_program);
+                if tcc.compile_string(&g).is_err() {
+                    cli::critical("could not compile C backend source-code. This is likely a bug");
+                    cli::information("if so, please report the bug at https://github.com/elo-lang/elo/issues");
+                    std::process::exit(-1);
+                }
+                let arguments =
+                    arguments.iter().map(|x| x.as_str()).collect::<Vec<&str>>();
+                std::process::exit(tcc.run(&arguments));
             } else {
                 cli::fatal(&format!("could not read input file {}", input));
                 std::process::exit(-1);
