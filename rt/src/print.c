c7c1cab8b955470c57c98411014829358019e4f1
marc-dantas <marciodantasms@hotmail.com>
2026-03-08 17:42:01 -0300

    rt: made print add a newline at the end of passed string

--- rt/src/print.c
+++ rt/src/print.c
@@ -4,5 +4,5 @@
 void __elo_print_str(MemoryContext* ctx, Str str) {
     char* ptr = __elo_handle_get(ctx, str.slot);
     ptr += str.offset;
-    printf("%.*s", str.size, ptr);
+    printf("%.*s\n", str.size, ptr);
 }
