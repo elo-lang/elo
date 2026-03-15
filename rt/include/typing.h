b62942abdc48bea9c5211114291f2868e9be6933
marc-dantas <marciodantasms@hotmail.com>
2026-03-14 21:41:00 -0300

    rt: created typing.h to centralize type definitions in Elo

---
+++ rt/include/typing.h
@@ -0,0 +1,23 @@
+// Elo's type definition
+
+#ifndef TYPING_H
+#define TYPING_H
+
+#define _ELO_I64_T   int64_t
+#define _ELO_I32_T   int32_t
+#define _ELO_I16_T   int16_t
+#define _ELO_I8_T    int8_t
+#define _ELO_BOOL_T  bool
+#define _ELO_U64_T   uint64_t
+#define _ELO_U32_T   uint32_t
+#define _ELO_U16_T   uint16_t
+#define _ELO_U8_T    uint8_t
+#define _ELO_F32_T   float
+#define _ELO_F64_T   double
+#define _ELO_INT_T   int32_t
+#define _ELO_UINT_T  uint32_t
+#define _ELO_FLOAT_T float
+#define _ELO_STR_T   Str
+#define _ELO_CHAR_T  uint32_t
+
+#endif
