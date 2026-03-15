6f0ecaee5dbb80fc2e322420d714fd9c6a6c982f
marc-dantas <marciodantasms@hotmail.com>
2026-03-14 21:42:12 -0300

    rt: implemented prints for other primitive types

--- rt/include/print.h
+++ rt/include/print.h
@@ -4,6 +4,11 @@
 #ifndef PRINT_H
 #define PRINT_H
 
-void __elo_print_str(MemoryContext* ctx, Str str);
+void __elo_print_str(MemoryContext* ctx, _ELO_STR_T value);
+void __elo_print_decimal(MemoryContext* ctx, _ELO_F64_T value);
+void __elo_print_unsigned(MemoryContext* ctx, _ELO_I64_T value);
+void __elo_print_signed(MemoryContext* ctx, _ELO_U64_T value);
+void __elo_print_bool(MemoryContext* ctx, _ELO_BOOL_T value);
+void __elo_print_char(MemoryContext* ctx, _ELO_CHAR_T value);
 
 #endif
--- rt/src/print.c
+++ rt/src/print.c
@@ -1,8 +1,50 @@
 #include <mem.h>
 #include <str.h>
 
-void __elo_print_str(MemoryContext* ctx, Str str) {
+void __elo_print_str(MemoryContext* ctx, _ELO_STR_T value) {
     char* ptr = __elo_handle_get(ctx, str.slot);
     ptr += str.offset;
     printf("%.*s\n", str.size, ptr);
+}
+
+void __elo_print_decimal(MemoryContext* ctx, _ELO_F64_T value) {
+    printf("%g\n", value);
+}
+
+void __elo_print_unsigned(MemoryContext* ctx, _ELO_I64_T value) {
+    printf("%llu\n", value);
+}
+
+void __elo_print_signed(MemoryContext* ctx, _ELO_U64_T value) {
+    printf("%ll\n", value);
+}
+
+void __elo_print_bool(MemoryContext* ctx, _ELO_BOOL_T value) {
+    printf("%s\n", value ? "true" : "false");
+}
+
+void __elo_print_char(MemoryContext* ctx, _ELO_CHAR_T value) {
+    uint32_t cp = value; // CP is for CODEPOINT for fuck sake
+    if (cp <= 0x7F) {
+        /* 1 byte: 0xxxxxxx (Standard ASCII) */
+        putchar((char)cp);
+    }
+    else if (cp <= 0x7FF) {
+        /* 2 bytes: 110xxxxx 10xxxxxx */
+        putchar((char)(0xC0 | (cp >> 6)));
+        putchar((char)(0x80 | (cp & 0x3F)));
+    }
+    else if (cp <= 0xFFFF) {
+        /* 3 bytes: 1110xxxx 10xxxxxx 10xxxxxx */
+        putchar((char)(0xE0 | (cp >> 12)));
+        putchar((char)(0x80 | ((cp >> 6) & 0x3F)));
+        putchar((char)(0x80 | (cp & 0x3F)));
+    }
+    else if (cp <= 0x10FFFF) {
+        /* 4 bytes: 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx */
+        putchar((char)(0xF0 | (cp >> 18)));
+        putchar((char)(0x80 | ((cp >> 12) & 0x3F)));
+        putchar((char)(0x80 | ((cp >> 6) & 0x3F)));
+        putchar((char)(0x80 | (cp & 0x3F)));
+    }
 }
