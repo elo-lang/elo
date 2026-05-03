3e6e5a4df4a73330799f2342dc3d358d4c5d7da2
marc-dantas <marciodantasms@hotmail.com>
2026-05-03 14:55:44 -0300

    rt: updated build system to compile for Windows (mingw)

--- rt/nob.c
+++ rt/nob.c
@@ -2,20 +2,30 @@
 #include "nob.h"
 
 #define BUILD_DIR "bin/"
-#define RT "libelort.a"
-#define ENTRY "src/main.c"
+#define RT_NATIVE  "libelort.a"
+#define RT_WIN     "elort.lib"
+#define ENTRY      "src/main.c"
 
 static Cmd cmd = {0};
 
-bool build_rt(const char* source) {
-	cmd_append(&cmd, "cc", "-c", "-o", BUILD_DIR RT, source);
-	cmd_append(&cmd, "-I", "include");
-	return cmd_run(&cmd);
+bool build_native(const char *source) {
+    cmd_append(&cmd, "cc", "-c", "-o", BUILD_DIR RT_NATIVE, source);
+    cmd_append(&cmd, "-I", "include");
+    return cmd_run(&cmd);
 }
 
-int main(int argc, char** argv) {
-	NOB_GO_REBUILD_URSELF(argc, argv);
-	if (!mkdir_if_not_exists(BUILD_DIR)) return 1;
-	if (!build_rt(ENTRY)) return 1;
-	return 0;
+bool build_mingw(const char *source) {
+    cmd_append(&cmd, "x86_64-w64-mingw32-gcc", "-c", "-o", BUILD_DIR RT_WIN, source);
+    cmd_append(&cmd, "-I", "include");
+    return cmd_run(&cmd);
+}
+
+int main(int argc, char **argv) {
+    NOB_GO_REBUILD_URSELF(argc, argv);
+
+    if (!mkdir_if_not_exists(BUILD_DIR)) return 1;
+    if (!build_native(ENTRY)) return 1;
+    if (!build_mingw(ENTRY)) return 1;
+
+    return 0;
 }
